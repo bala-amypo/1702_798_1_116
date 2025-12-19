8c295623d78ba330ba179d17931935ace770fd3a
bala-amypo <bala@amypo.in>
2025-12-19 05:09:05 +0000

    User commit

--- src/main/java/com/example/demo/repository/BreachReportRepository.java
+++ src/main/java/com/example/demo/repository/BreachReportRepository.java
@@ -1,3 +1,12 @@
-public interface BreachReportRepository extends JpaRepository<BreachReport, Long> {
+package com.example.demo.repository;
+
+import com.example.demo.entity.BreachReport;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BreachReportRepository
+        extends JpaRepository<BreachReport, Long> {
+
     List<BreachReport> findByContractId(Long contractId);
 }
